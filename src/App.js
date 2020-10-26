2fa31a68f18ed8070964550e5b6ea4af968e4617
chandu-lanka <69030446+chandu-lanka@users.noreply.github.com>
2020-10-26 12:04:07 +0530

    Just made a little chat thing might work on it later

--- src/App.js
+++ src/App.js
@@ -1,10 +1,61 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./styles.css";
+import {
+  Button,
+  Input,
+  FormControl,
+  InputLabel,
+  Card,
+  CardContent,
+  Typography
+} from "@material-ui/core";
+import Message from "./Message";
 
 export default function App() {
+  const [isTyping, setIsTyping] = useState("");
+  const [messages, setMessages] = useState([]);
+  const [username, setUsername] = useState("");
+
+  useEffect(() => {
+    setUsername(prompt("Please enter your name for the live chat"));
+  }, []);
+
+  const SendMessages = (event) => {
+    event.preventDefault();
+    setMessages([...messages, { username: username, text: isTyping }]);
+    setIsTyping("");
+  };
+
   return (
     <div className="App">
-      <h4>Sup Boys</h4>
+      <h1>FaceBook Messenger Clone</h1>
+      <h2>welcome {username}</h2>
+      <form>
+        <FormControl>
+          <InputLabel>Send A Message..</InputLabel>
+          <Input
+            value={isTyping}
+            onChange={(event) => setIsTyping(event.target.value)}
+          />
+
+          <Button
+            disabled={!isTyping}
+            variant="contained"
+            color="primary"
+            onClick={SendMessages}
+            type="submit"
+          >
+            Send Message
+          </Button>
+        </FormControl>
+      </form>
+      {messages.map((messages) => (
+        <div className="MessageContainer">
+          <h2>
+            {messages.username}: {messages.text}
+          </h2>
+        </div>
+      ))}
     </div>
   );
 }
